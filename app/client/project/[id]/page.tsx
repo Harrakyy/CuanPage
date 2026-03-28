11fc9485d7b667073fe74e7bb0dbd9dc4c8725c6
Harrakyy <166911147+Harrakyy@users.noreply.github.com>
2026-03-28 14:08:23 +0700

    update export2

--- app/client/project/[id]/page.tsx
+++ app/client/project/[id]/page.tsx
@@ -1,5 +1,5 @@
-'use client'
 export const dynamic = 'force-dynamic'
+'use client'
 import { createClient } from '@/lib/supabase/client'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
